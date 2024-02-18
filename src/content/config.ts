279106b7af39ec14d9dfe3b4628b25b18a511e21
lefpap <lolopap21@gmail.com>
2024-02-18 18:46:29 +0200

    Add Astro Icon integration and update dependencies

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,6 @@ import { defineCollection, z } from "astro:content";
 const projectsCollection = defineCollection({
   type: "content",
   schema: {
-    slug: z.string(),
     title: z.string(),
     description: z.string(),
     status: z.enum(["active", "completed", "on-hold"]),
@@ -12,19 +11,20 @@ const projectsCollection = defineCollection({
     endDate: z.date().optional(),
     repo: z.string().optional(),
     live: z.string().optional(),
+    selected: z.boolean().optional(),
   },
 });
 
 const postsCollection = defineCollection({
   type: "content",
   schema: {
-    slug: z.string(),
     title: z.string(),
     description: z.string(),
     publishedDate: z.date(),
     images: z.array(z.string()).optional(),
     status: z.enum(["draft", "published"]),
-    tags: z.array(z.string()).min(1).max(5),
+    tags: z.array(z.string()),
+    selected: z.boolean().optional(),
   },
 });
 
